966509d1c10c7f5b4e6b2eec122312a15bbbb3f0
Neil Armstrong <narmstrong@baylibre.com>
2019-01-21 15:51:48 +0100

    clk: meson: Add G12A AO Clock + Reset Controller
    Add the Amlogic G12A AO Clock and Reset controller driver handling
    generation of Always-On clocks :
    - AO Clocks and Reset for Always-On modules
    - 32K Generation for USB and CEC
    - SAR ADC controller clock
    
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>

---
+++ drivers/clk/meson/g12a-aoclk.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
+/*
+ * Copyright (c) 2019 BayLibre, SAS
+ * Author: Neil Armstrong <narmstrong@baylibre.com>
+ */
+
+#ifndef __G12A_AOCLKC_H
+#define __G12A_AOCLKC_H
+
+#define NR_CLKS	29
+
+#include <dt-bindings/clock/g12a-aoclkc.h>
+#include <dt-bindings/reset/g12a-aoclkc.h>
+
+#endif /* __G12A_AOCLKC_H */
