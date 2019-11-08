cb7fbf4a8d6eabfd9d916f290a003b2fd04ff373
Neil Armstrong <narmstrong@baylibre.com>
2019-11-08 10:54:45 +0100

    Add gitlab-ci
    Change-Id: I928bfb95844f53cc19c0c081e65906b99d0ebc25

---
+++ gitlab-ci/bbci.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -xe
+
+BBCI_PATH=$1
+SOURCE_NAME=$2
+TARGET_NAME=$3
+
+BBCI_OPTS="-d --nolog -s $SOURCE_NAME -t $TARGET_NAME"
+
+cd $BBCI_PATH
+
+echo "Cloning Linux..."
+./bbci.py $BBCI_OPTS -a create
+
+echo "Downloading Toolchain"
+./bbci.py $BBCI_OPTS -a download
+
+echo "Building kernel"
+./bbci.py $BBCI_OPTS --configoverlay vanilla --noclean -a build
+
+echo "Generating LAVA test jobs"
+./bbci.py $BBCI_OPTS --configoverlay vanilla --noact -a boot
+
+exit 0
