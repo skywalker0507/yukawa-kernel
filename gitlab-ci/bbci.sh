55fd36a5913eaf808f38b22765448ee927816dfd
Neil Armstrong <superna9999@gmail.com>
2019-11-18 15:13:21 +0000

    Update gitlab-ci/Dockerfile, gitlab-ci/bbci.sh, gitlab-ci/sources.yaml, .gitlab-ci.yml files

--- gitlab-ci/bbci.sh
+++ gitlab-ci/bbci.sh
@@ -11,9 +11,6 @@ BBCI_OPTS="-d --nolog -s $SOURCE_NAME -t $TARGET_NAME -D $BOARD_TAG"
 
 cd $BBCI_PATH
 
-#echo "Cloning Linux..."
-#./bbci.py $BBCI_OPTS -a create
-
 echo "Downloading Toolchain"
 ./bbci.py $BBCI_OPTS -a download
 
